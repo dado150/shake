1c6cbb0c5bc1b2eb3e7c44705382e38b15f76c18
DAdo150 <davide@150up.com>
2015-12-23 20:08:29 +0100

    Random video at Shake

--- shake/ViewController.swift
+++ shake/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+   
+    override func motionEnded(motion: UIEventSubtype,
+        withEvent event: UIEvent?) {
+            
+            if motion == .MotionShake{
+                
+                let number: Int = 50
+                let randomNumber = Int(arc4random_uniform(UInt32(number)))
+                print(randomNumber)
+                
+                
+                let url = NSURL(string: "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUi8e0iOVk1fEOogdfu4YgfA&key=AIzaSyAJjIyppyws7NCRVBwKYWBjw1mAkpD05wQ")
+                
+                let session = NSURLSession.sharedSession()
+                
+                let task = session.dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                    
+                    if error != nil {
+                        
+                        print(error)
+                        
+                    } else {
+                        
+                        var err: NSError?
+                        
+                        var jsonResult = (try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
+                        
+                        if err != nil {
+                            
+                            print(err)
+                            
+                        } else {
+                            
+                            var first = jsonResult["items"] as! NSArray
+                            var arrayVideo = first[randomNumber] as! NSDictionary
+                            var getInfoVideo = arrayVideo["snippet"] as! NSDictionary
+                            var getResources = getInfoVideo["resourceId"] as! NSDictionary
+                            var getVideoID = getResources["videoId"] as! String
+                            print(getVideoID)
+                            
+                        }
+                        
+                    }
+                    
+                })
+                
+                task.resume()
+            }
+            
+    }
+    
+    
 
 }
 
