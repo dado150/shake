b001ff9bd4103697d1e5a53ac66686a1889f8599
DAvide Colla <dado150@gmail.com>
2015-12-28 02:36:25 +0100

    Done List of movie and Shake functions!

--- shake/ViewController.swift
+++ shake/ViewController.swift
@@ -14,40 +14,64 @@ import CoreData
 import YouTubePlayer
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var YouTubePlayer: YouTubePlayerView!
+    @IBOutlet weak var titleVideo: UILabel!
+    
+    var arrayVideo = NSDictionary()
+    var getInfoVideo = NSDictionary()
+    var getResources = NSDictionary()
+    var getVideoID = String()
+    var titolo = String()
+    var FinalTitle = String()
+    
+
+    struct MovieBlock {
+        var movieID = String()
+        var displayTitle = String()
+        
+    }
+    
+    var movieList: [MovieBlock] = []
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let url = NSURL(string: "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUi8e0iOVk1fEOogdfu4YgfA&key=AIzaSyAJjIyppyws7NCRVBwKYWBjw1mAkpD05wQ")
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+            if error != nil {
+                print(error)
+            } else {
+                var err: NSError?
+                var jsonResult = (try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
+                if err != nil {
+                    print(err)
+                } else {
+                    var first = jsonResult["items"] as! NSArray
+                    for var index = 0; index < 49 ; index++ {
+                        self.arrayVideo = first[index] as! NSDictionary
+                        self.getInfoVideo = self.arrayVideo["snippet"] as! NSDictionary
+                        self.getResources = self.getInfoVideo["resourceId"] as! NSDictionary
+                        self.getVideoID = self.getResources["videoId"] as! String
+                        self.titolo = (self.getInfoVideo["title"] as? String)!
+                        self.shortenTitle(self.titolo)
+                        
+                        var movieBox:MovieBlock = MovieBlock(movieID: self.getVideoID, displayTitle: self.FinalTitle)
+                        self.movieList.append(movieBox)
+                        
+                    }
+                }
 
-       
-//        
-//        var appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-//        var context:MSManagedObjectContext = appDel.managedObjectContext!
-//        
-//        var newMovie = NSEntityDescription.insertNewObjectForEntityForName("My Movies", inManagedObjectContext: context) as MSManagedObject
-//        
-//        newMovie.setValue("Star Wars", forKey: "title")
-    
-        
-        
-//        let videoURL = NSURL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
-//        let player = AVPlayer(URL: videoURL!)
-//        let playerLayer = AVPlayerLayer(player: player)
-//        playerLayer.frame = CGRectMake(10, 100 , 100, 100)
-//        self.view.layer.addSublayer(playerLayer)
-//        player.play()
-        
-//        let address = NSURL(string: "https://www.youtube.com/watch?v=SEyDminvr7Q")
-        
+            }
+            
+        })
+        task.resume()
     }
-    
-    @IBOutlet weak var YouTubePlayer: YouTubePlayerView!
-       
-    @IBOutlet weak var video_Player: UIWebView!
-    
-    
-    
-    @IBOutlet weak var titleVideo: UILabel!
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -55,79 +79,46 @@ class ViewController: UIViewController {
     }
    
     override func motionEnded(motion: UIEventSubtype,withEvent event: UIEvent?) {
-            
+        
             if motion == .MotionShake{
                 
-                let number: Int = 50
+                let number: Int = 49
                 let randomNumber = Int(arc4random_uniform(UInt32(number)))
-                print(randomNumber)
                 
+                var pickedMovie = movieList[randomNumber]
                 
-                let url = NSURL(string: "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=UUi8e0iOVk1fEOogdfu4YgfA&key=AIzaSyAJjIyppyws7NCRVBwKYWBjw1mAkpD05wQ")
+                self.YouTubePlayer.playerVars = [
+                    "playsinline": "1",
+                    "controls": "1",
+                    "showinfo": "0"
+                ]
                 
-                let session = NSURLSession.sharedSession()
-                
-                let task = session.dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
-                    
-                    if error != nil {
-                        
-                        print(error)
-                        
-                    } else {
-                        
-                        var err: NSError?
-                        
-                        var jsonResult = (try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
-                        
-                        if err != nil {
-                            
-                            print(err)
-                            
-                        } else {
-                            
-                            var first = jsonResult["items"] as! NSArray
-                            var arrayVideo = first[randomNumber] as! NSDictionary
-                            var getInfoVideo = arrayVideo["snippet"] as! NSDictionary
-                            var getResources = getInfoVideo["resourceId"] as! NSDictionary
-                            var getVideoID = getResources["videoId"] as! String
-                            var title = getInfoVideo["title"] as! String
-                            
-                            self.YouTubePlayer.playerVars = [
-                                "playsinline": "1",
-                                "controls": "1",
-                                "showinfo": "0"
-                            ]
-                            
-                            self.YouTubePlayer.loadVideoID(getVideoID)
-
-                            print(title)
-                            var videoFullTitle = title
-      
-                                if videoFullTitle.rangeOfString("Trailer") != nil {
-                                
-                                    var delimiter = " Official"
-                                    var newstr = videoFullTitle
-                                    var shortTitle = newstr.componentsSeparatedByString(delimiter)
-                                    var FinalTitle = shortTitle[0]
-                                    self.titleVideo.text = FinalTitle
-                                    print(FinalTitle)
-                        
-                                } else{
-                                    self.titleVideo.text = title
-                                }
-                       }
-                        
-                    }
-                    
-                })
-                
-                task.resume()
+                self.YouTubePlayer.loadVideoID(pickedMovie.movieID)
+                self.titleVideo.text = pickedMovie.displayTitle
             }
-            
+        
     }
     
+    func shortenTitle(realTitle:String) ->String{
+        
+        if realTitle.rangeOfString("Official") != nil {
+            
+            var delimiter = " Official"
+            var newstr = realTitle
+            var shortTitle = newstr.componentsSeparatedByString(delimiter)
+            FinalTitle = shortTitle[0]
+            //                            self.titleVideo.text = FinalTitle
+            
+        } else{
+            FinalTitle = realTitle
+            
+        }
+        return FinalTitle
+    }
     
+    func readingJson (){
     
+    }
 
 }
 
